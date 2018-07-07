920d7a23cfa9bc4a501db21d9ffa954fe9c05cbf
spasartyom <spasartyom@gmail.com>
2018-07-08 01:52:56 +0300

    redone rand

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,16 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
-import java.io.File;
-
-import java.io.*;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.StringReader;
-
 import javax.swing.JButton;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,6 +14,8 @@ public class Launcher extends JFrame{
 
     private JLabel labelWeight;
     private JSpinner spinner;
+    private JLabel labelEdges;
+    private JSpinner spinnerEdges;
     private JLabel descLabel;
     private JLabel resLabel;
     private JLabel label;
@@ -29,18 +25,12 @@ public class Launcher extends JFrame{
     private JButton buttonNext;
     private JButton buttonRand;
     private JButton buttonCheck;
-
     private Canvas canvas;
-
-    //Graphics g;
-
     private InputOutput ios;
     private Graph gr;
     private MST mst;
-
     private JPanel rootPanel;
 
-    // ключевое слово super, которое обозначает суперкласс, т.е. класс, производным от которого является текущий класс
     public Launcher() {
         this.setSize(800,550);
         this.setResizable(false);
@@ -56,8 +46,8 @@ public class Launcher extends JFrame{
 
         Font font = new Font("Consolas", 0, 13);
 
-        this.spinner = new JSpinner();
-        this.spinner.setValue(7);
+        SpinnerModel modelVertex = new SpinnerNumberModel(7,2,100,1);
+        this.spinner = new JSpinner(modelVertex);
         this.spinner.setBounds(112,12,38,28);
         this.spinner.setVisible(true);
 
@@ -67,10 +57,21 @@ public class Launcher extends JFrame{
         this.labelWeight.setBounds(12,18,108,14 );
         this.labelWeight.setVisible(true);
 
+        SpinnerModel modelEdges = new SpinnerNumberModel(1,1,4950,1);
+        this.spinnerEdges = new JSpinner(modelEdges);
+        this.spinnerEdges.setBounds(112,52,38,28);
+        this.spinnerEdges.setVisible(true);
+
+        this.labelEdges = new JLabel();
+        this.labelEdges.setText("Num of edges:");
+        this.labelEdges.setFont(font);
+        this.labelEdges.setBounds(12,58,108,14 );
+        this.labelEdges.setVisible(true);
+
         this.label = new JLabel();
         this.label.setText("Graph data:");
         this.label.setFont(font);
-        this.label.setBounds(12,44,85,14);
+        this.label.setBounds(12,84,85,14);
         this.label.setVisible(true);
 
         //кнопки управления
@@ -89,11 +90,11 @@ public class Launcher extends JFrame{
 
         //устанавливаем размеры кнопок
 
-        this.buttonGraph.setBounds(12,260,66,24);
-        this.buttonResult.setBounds(86,260,66,24);
-        this.buttonRand.setBounds(12,292,66,24);
-        this.buttonNext.setBounds(86,292,66,24);
-        this.buttonCheck.setBounds(12,324,66,24);
+        this.buttonGraph.setBounds(12,300,66,24);
+        this.buttonResult.setBounds(86,300,66,24);
+        this.buttonRand.setBounds(12,332,66,24);
+        this.buttonNext.setBounds(86,332,66,24);
+        this.buttonCheck.setBounds(12,364,66,24);
 
         //убираем выделение текста кнопки
 
@@ -106,7 +107,7 @@ public class Launcher extends JFrame{
 
         //поясняющие надписи
         this.descLabel = new JLabel();
-        this.descLabel.setBounds(166,450,418,48);
+        this.descLabel.setBounds(166,450,618,48);
         this.descLabel.setText("Description: ");
         this.descLabel.setFont(font);
 
@@ -116,7 +117,7 @@ public class Launcher extends JFrame{
         this.resLabel.setFont(font);
         //строка для указания кол-ва вершин
         this.dataGraph = new JTextArea("");
-        this.dataGraph.setBounds(12,64,140,1840);
+        this.dataGraph.setBounds(12,104,140,1840);
         this.dataGraph.setText("0 1 7\n" +
                 "0 3 4\n" +
                 "1 3 9\n" +
@@ -129,7 +130,7 @@ public class Launcher extends JFrame{
                 "4 6 8\n" +
                 "5 6 13");
         JScrollPane scroll = new JScrollPane(dataGraph);
-        scroll.setBounds(12,64,140,184);
+        scroll.setBounds(12,104,140,184);
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
@@ -162,6 +163,8 @@ public class Launcher extends JFrame{
         this.rootPanel.add(this.resLabel);
         this.rootPanel.add(this.spinner);
         this.rootPanel.add(this.labelWeight);
+        this.rootPanel.add(this.labelEdges);
+        this.rootPanel.add(this.spinnerEdges);
 
         buttonResult.setEnabled(false);
         buttonNext.setEnabled(false);
@@ -182,7 +185,7 @@ public class Launcher extends JFrame{
                     resLabel.setText("Result: -");
                     buttonResult.setEnabled(true);
                     buttonNext.setEnabled(true);
-                    buttonGraph.setEnabled(false);
+                    buttonGraph.setEnabled(true);
                     buttonCheck.setEnabled(true);
                 } catch (Exception e) {
                     descLabel.setText("Description: exception! "+e.getClass().getName()+": "+e.getMessage());
@@ -233,41 +236,41 @@ public class Launcher extends JFrame{
 
         buttonRand.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
-                int num = (int) (3 + Math.random()*9);
-                spinner.setValue(num);
+                //int num = (int) (2 + Math.random()*9);//spinner.setValue(num);
+                int num = (int) spinner.getValue();
                 dataGraph.setText("");
-                int numEdg = 0;
-                for(int i = 0; i <= num; ++i){
-                    numEdg += i;
+                int numEdg1 = 0;
+                for(int i = 0; i < num; ++i){
+                    numEdg1 += i;
                 }
+                int numEdg = (int) spinnerEdges.getValue();
+                if (numEdg > numEdg1) numEdg = numEdg1;
+                int[] v1 = new int[numEdg1];
+                int[] v2 = new int[numEdg1];
+                double[] w = new double[numEdg1];
 
-                int[] v1 = new int[numEdg];
-                int[] v2 = new int[numEdg];
-                double[] w = new double[numEdg];
-                UnionField uf = new UnionField(num);
-
-                for(int i=0; uf.count()!=1 ; i++){
+                for(int i = 0; i < numEdg; i++){
                     w[i] = Math.random()*1000;
                     do{
-                        v1[i] = (int) (0+Math.random()*(num));
-                        v2[i] = (int) (0+Math.random()*(num));
-                    }while((v1[i]==v2[i]));
+                        v1[i] = (int) (0 + Math.random() * num);
+                        v2[i] = (int) (0 + Math.random() * num);
+                    }while((v1[i] == v2[i]));
                     for (int j = 0; j < i; j++) {
                         if ((v1[i] == v1[j] && v2[i] == v2[j]) || (v1[i] == v2[j] && v2[i] == v1[j])) {
                             do {
-                                v1[i] = (int) (0 + Math.random() * (num));
-                                v2[i] = (int) (0 + Math.random() * (num));
+                                v1[i] = (int) (0 + Math.random() * num);
+                                v2[i] = (int) (0 + Math.random() * num);
                             }
                             while ((v1[i] == v2[i])
                                     || ((v1[i] == v1[j] && v2[i] == v2[j]) || (v1[i] == v2[j] && v2[i] == v1[j])));
+                            j = -1;
                         }
                     }
-                    uf.unoin(v1[i], v2[i]);
                     int tmp = (int) w[i];
-                    w[i] = 1+((double)tmp)/20;
-                    dataGraph.append(""+v1[i]+" ");
-                    dataGraph.append(""+v2[i]+" ");
-                    dataGraph.append(""+w[i]+"\n");
+                    w[i] = 1 + ((double)tmp) / 20;
+                    dataGraph.append("" + v1[i] + " ");
+                    dataGraph.append("" + v2[i] + " ");
+                    dataGraph.append("" + w[i] + "\n");
                 }
                 buttonResult.setEnabled(false);
                 buttonNext.setEnabled(false);
